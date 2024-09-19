319c7f62dd69f21f05a4252b1b9cb05ceb23ee1b
iwasawarenji954 <iwasawarenji954@gmail.com>
2024-09-19 23:58:08 +0900

    updata: タイトルの検索機能

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -17,8 +17,10 @@ import {
     PopoverContent,
 } from "@/components/ui/popover"
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 export const Navigation = () => {
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const isResizingRef = useRef(false);
@@ -139,7 +141,7 @@ export const Navigation = () => {
                         label="Search"
                         icon={Search}
                         isSearch
-                        // onClick={() => ()}
+                        onClick={search.onOpen}
                     />
                     <Item
                         label="Setting"
